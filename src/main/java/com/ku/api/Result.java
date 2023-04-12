988eefb4ac9ba182519c19b2b30740a2508b75fd
quick <1605009393@qq.com>
2023-04-12 11:07:21 +0800

    更新无参构造函数的问题

--- src/main/java/com/ku/api/Result.java
+++ src/main/java/com/ku/api/Result.java
@@ -1,10 +1,14 @@
 package com.ku.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Result<T> {
 
     private boolean state;
